38e3e13db37692fa75c107aba76a7bea4d97a319
Marco Duman <marcodum72@gmail.com>
2024-02-16 10:05:34 -0500

    Basic Bluetooth connection working

--- Robot/main.py
+++ Robot/main.py
@@ -1,25 +1,21 @@
 #!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import Port, Stop, Direction, Button, Color
-from pybricks.tools import wait, StopWatch, DataLog
-from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
+from pybricks.messaging import BluetoothMailboxServer, TextMailbox
 
-
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
-
-
-# Create your objects here.
 ev3 = EV3Brick()
-obstacle_sensor = UltrasonicSensor(Port.S4)
-sensor = InfraredSensor(Port.S3)
-
-# Write your program here.
 ev3.speaker.beep()
 
-while True :
-    print(sensor.distance())
+server = BluetoothMailboxServer()
+mbox = TextMailbox('greeting', server)
+
+# The server must be started before the client!
+print('waiting for connection...')
+server.wait_for_connection()
+print('connected!')
+
+# In this program, the server waits for the client to send the first message
+# and then sends a reply.
+mbox.wait()
+print(mbox.read())
+mbox.send('hello to you!')
 
