47ce30c688d40c746d25fc97f8283326c6da7b24
LeGhosdragon <simon.bou@bgp.ca>
2024-03-03 14:09:19 -0500

    all ok

--- Robot/main.py
+++ Robot/main.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
@@ -6,7 +5,7 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
-
+import math
 from drivebase import Drivebase
 from sensors import Sensors
 
@@ -16,23 +15,98 @@ from sensors import Sensors
 
 # Create your objects here.
 ev3 = EV3Brick()
-s = Sensors()
 d = Drivebase()
 d._init_()
+#x : float = 0
+#y : float = 0
+#dist est le rapport de déplacement sur le temps utilisé pour le faire
+#dist : float = 0
+
+#pos = [x,y]
+
 # Write your program here.
+#ev3.speaker.beep()
 
 #d.setSpeed(200)
 #20.6  2 rotations
-#9.745    1 rotation
+#9.745 rotations = 102.616 cm   1 rotation
 #40.4  10 rotations
 #d.turn(90, 100)
 #d.turn(-180, 100)
-d.moveAuto(s)
-while True:
-    s.update()
+
+#cette fonction reçoit dist : le rapport de déplacement sur un temps déterminé, et reçoit angle : la valeur que le gyro retourne.
+def newPos(dist : float, angle : float):
+    x = x + math.cos(angle % 360) * dist, 
+    y = y + math.sin(angle % 360) * dist
     
+#Cette fonction reçoit la distance en centimètres et retourne le nombre de degrés que les moteurs doivent tourner
+def cmToAngleRot(dist : float): 
+    return ((dist * 0.0949) * 360)
+
+#def updateDist():
+    
+
+#x : float = obstacle_sensor.distance()
+
+#while (True):
+    #print(float(x))
     #print(d._kGyro.angle())
-    #d._kLeftMotor.run(200)
-    #d._kRightMotor.run(-200)
-    #if d._kLeftMotor.angle() >= 360*float(9.745):
-    #    d.stop()
+    #d._kLeftMotor.run(360)
+    #d._kRightMotor.run(360)
+    #if d._kLeftMotor.angle() >= cmToAngleRot(127):
+        #d.stop()
+
+def ligneDroite(speedLvl, distance):
+    baseAngle = d._kGyro.angle()
+    baseSpeed = -speedLvl * 90
+    while (True):
+        print(d._kGyro.angle())
+        print(baseAngle)
+        #baseAngle = baseAngle + 0.0007
+        if d._kGyro.angle() > baseAngle: 
+            d._kLeftMotor.run(baseSpeed - 10 )
+            d._kRightMotor.run(baseSpeed + 10)
+        elif d._kGyro.angle() < baseAngle:
+            d._kLeftMotor.run(baseSpeed + 10)
+            d._kRightMotor.run(baseSpeed - 10)
+        else:
+            d._kLeftMotor.run(baseSpeed)
+            d._kRightMotor.run(baseSpeed)
+        #if d._kLeftMotor.angle() >= cmToAngleRot(30):
+            #d.stop()
+
+def ligneDroiteSans(speedLvl, distance):
+    baseSpeed = -speedLvl * 90
+    while (True):
+        d._kLeftMotor.run(baseSpeed)
+        d._kRightMotor.run(baseSpeed)
+
+def testerGyro(x, y , z):
+    baseAngle = d._kGyro.angle()
+    while (x < 300):
+        print(d._kGyro.angle())
+        print(baseAngle)
+        #print(x)
+        if d._kGyro.angle() > baseAngle: 
+            d._kLeftMotor.run(- 10 )
+            d._kRightMotor.run(+ 10)
+        elif d._kGyro.angle() < baseAngle:
+            d._kLeftMotor.run(+ 10)
+            d._kRightMotor.run(- 10)
+        x = x + 1
+    while (y < 50000):
+        print(d._kGyro.angle())
+        #print(baseAngle)
+        #print(y)
+        if y%10000 < 5000: 
+            d._kLeftMotor.run(- 60 )
+            d._kRightMotor.run(+ 60)
+        elif y%10000 > 5000:
+            d._kLeftMotor.run(+ 60)
+            d._kRightMotor.run(- 60)
+        y = y + 1
+    
+
+#testerGyro(0,0,0)
+#ligneDroite(3,0)
+ligneDroiteSans(3,0)
