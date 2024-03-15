8ef995f1cf03eb4c83058e591ec1e63d603a4d05
Allison <shkodra.allison@gmail.com>
2024-03-15 12:24:47 -0400

    Les données sont séparer dans des catégories

--- Mapping/TraitementDonnees.py
+++ Mapping/TraitementDonnees.py
@@ -2,9 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
 
-#Sert à représenter les données qui seront reçus par le robot (À ENLEVER APRÈS)
-data = []
 
+data = [] # une liste contenant des points
+
+#Sert à représenter les données qui seront reçus par le robot (À ENLEVER APRÈS)
+# on ajoute dans la liste data les coordonées du point dans le format suivant [x,y]
 data.append([2.100125603600003,14.230716153411098])
 data.append([2.646198640079976,14.206973847477187])
 data.append([3.0973024528243016,14.230716153411098])
@@ -1252,8 +1254,6 @@ data.append([19.788143524364347,14.017035400005891])
 
 
 espacement = 0.1
-print(data[0])
-print(data[0][0])
 
 def trouverMaxX(data):  #Pour trouver la valeur maximale en X
     maxX = data[0][0]
@@ -1306,7 +1306,7 @@ class grilleSalle: #Classe du grillage pour que chaque case ait les variables su
 def creerGrille(data): #métode pour creer un grillage et séparer les données
     #création de la grille
     grille = [[grilleSalle for i in range(int((trouverMaxY(data)-trouverMinY(data))/espacement)+1)] for j in range(int((trouverMaxX(data)-trouverMinX(data))/espacement)+1)] 
-    
+
     #initialisation des pointeurs
     pointeurLimSuppX = trouverMinX(data) 
     pointeurLimInfX = trouverMinX(data)
@@ -1334,12 +1334,16 @@ def creerGrille(data): #métode pour creer un grillage et séparer les données
             grille[i][j].limiteInfX = pointeurLimInfX
             grille[i][j].limiteSuppY = pointeurLimSuppY
             grille[i][j].limiteInfY = pointeurLimInfY
-            print("Point (", i, ", ", j, ") : LimiteSuppX = ", grille[i][j].limiteSuppX, "; LimiteInfX = ", grille[i][j].limiteInfX, "; LimiteSuppY = ", grille[i][j].limiteSuppY, "; LimiteInfY = ",grille[i][j].limiteInfY)
-    
-   # for i in range(len(grille)): # on parcours les colonnes | du tableau grille 
-        #for j in range (len(grille[0])): # on parcours les lignes - du tableau grille
-             #for numPoint in range(len(data)): # on parcours les points des données originales
-                #if data[numPoint][0] > grille[i][j].limiteInfX 
+            #print("Point (", i, ", ", j, ") : LimiteSuppX = ", grille[i][j].limiteSuppX, "; LimiteInfX = ", grille[i][j].limiteInfX, "; LimiteSuppY = ", grille[i][j].limiteSuppY, "; LimiteInfY = ",grille[i][j].limiteInfY)
+
+            for point in range(len(data)): # on parcours les points des données originales
+                if (data[point][0] >= grille[i][j].limiteInfX and data[point][0] <= grille[i][j].limiteSuppX) and (data[point][1] >= grille[i][j].limiteInfY and data[point][1] <= grille[i][j].limiteSuppY):
+                    grille[i][j].dataGrille.append(data[point]) 
+
+            grille[i][j].quantite = len(grille[i][j].dataGrille)
+            print(grille[i][j].quantite)
+
+             
             
 
 
