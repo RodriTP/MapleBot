30473a1a8c15ecffc788146d551e873542b24c72
Marco Duman <marcodum72@gmail.com>
2024-04-05 13:42:28 -0400

    Verision non fonctionnelle, mais plus avancée

--- Mapping/testTraitementDonnes.py
+++ Mapping/testTraitementDonnes.py
@@ -1,22 +1,34 @@
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 from  GrilleSalle import grilleSalle
-from Data import Data
 ##################Début classe mainTraitementDonnees (sert de main) #######################
 #### creer une grille la remplis avec la date et affiche la grille (modifier ce commentaire si mauvaise description)
+_x = []
+_y = []
+fig, ax = plt.subplots()
 class mainTraitementDonnees :
-    d = Data
-    def __init__(self, data:Data):
-        d = data
-        
 
+    def __init__(self):
+        return
+    
+    def ajouterPoint(self, x:float, y:float):
+        global _x
+        global _y
+        _x.append(x)
+        _y.append(y)
+
+    def update(frame):
+        global ax
+        global fig
+        global _x
+        global _y
+        ax.clear()  # clearing the axes
+        ax.scatter(_x, _y)  # creating new scatter chart with updated data
+        fig.canvas.draw()  # forcing the artist to redraw itself
+
+
+"""
     def afficherGrille(self):
-        x = []
-        y = []
-
-        for point in range(len(Data.data)-1):
-            x.append(self.d.data[point][0])
-            y.append(self.d.data[point][1])
-
         plt.scatter(x,y) # Ajoute le nuage de point, c-à-d les données que le robot à collecter, au plot 
 
         #stats = linregress(data[:][0], data[:][1]) # Régression linéaire
@@ -25,4 +37,5 @@ class mainTraitementDonnees :
         #plt.ylim([10, 15])
 
         plt.show() # afficher le tout
+"""
 ######################################################################
