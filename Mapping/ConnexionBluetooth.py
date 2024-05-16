64d01ff1bd4046cd349ec4fbf6353ce4804a28c7
Marco Duman <marcodum72@gmail.com>
2024-05-16 19:21:50 -0400

    Ajout commentaire dans mainPC et connexionBluetooth

--- Mapping/ConnexionBluetooth.py
+++ Mapping/ConnexionBluetooth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from pcPybricks.messaging import BluetoothMailboxClient, TextMailbox
 
-
+#L'exemple parlé ci-dessous a été grandement modifié pour pouvoir fonctionner pour ce qu'on veuille faire
 
 # This demo makes your PC talk to an EV3 over Bluetooth.
 #
@@ -21,59 +21,78 @@ from pcPybricks.messaging import BluetoothMailboxClient, TextMailbox
 # This is the address of the server EV3 we are connecting to.
 
 class connexionBluetooth:
+    """
+    Objet qui gère et effectue l'envoie et la réception de données avec le ev3
+    Utilise le Bluetooth comme moyen de communication
+    """
     #Données qui seront reçues et envoyés
     dataToSend = []
     dataRecieved = []
 
     #L'adresse bluetooth du EV3
     SERVER = "00:17:ec:f4:9d:c8"
-    #Initilisation de la connection Bluetooth
+
+    #Initialisation des classes nécessaires pour créer la connexion
     client = BluetoothMailboxClient()
     mbox = TextMailbox("greeting", client)
 
     def __init__(self):
-        print("establishing connection...")
+        """
+        Initialise la connexion Bluetooth au robot
+        """
+        print("établissement connexion...")
         self.client.connect(self.SERVER)
-        print("connected!")
+        print("connecté!")
 
-        # In this program, the client sends the first message and then waits for the
-        # server to reply.
-        #Envoie de message
-        self.mbox.send("hello!")
+        # Au début, l'ordinateur envoie un message et attend la réponse du ev3
+        self.mbox.send("bonjour!")
         self.mbox.wait()
         #exemple de truc à recevoir
         print(self.mbox.read())
 
-    i = 1
+    nombreDechangeBluetooth = 1
 
     #L'échange de donnés qui doit se faire tout le temps
     def dataExchange(self):
         self.mbox.wait()
         self.dataRecieved.append(self.mbox.read())
-        #if(len(self.dataToSend)==0):
-        #    self.mbox.send('NULL')
-        #    print("send return")
-        #else:
-        reponse = "NULL"+ str(connexionBluetooth.i)
-        self.mbox.send(reponse)
-        #    print("WHYYYYYYY")
-        print(reponse)
-        connexionBluetooth.i+=1
+        if(len(self.dataToSend)==0):
+            self.mbox.send('NULL'+ str(connexionBluetooth.nombreDechangeBluetooth))
+        else:
+            self.mbox.send(self.dataToSend)
+            self.addDataToSend.clear()
+        connexionBluetooth.nombreDechangeBluetooth +=1
 
-    #Ajoute des données qui vont être transmises
     def addDataToSend(self, dataToSend):
+        """
+        Ajoute des données qui vont être transmises au robot
+        """
         self.dataToSend.append(dataToSend)
     
-    #Retourne toutes les données obtenues
     def getData(self):
+        """
+        Retourne toutes les données obtenues dans l'échange
+        """
         return self.dataRecieved
     
-    #Reset les données obtenues, à faire après les getter
     def resetData(self):
+        """
+        Reset les données obtenues, à faire après les getter
+        """
         self.dataRecieved.clear()
 
     def separateData(self):
-        return self.dataRecieved[0].split(";")
-    #prend ce que reçois l'ordi (x:200.2902) pour le séparer pour juste avoir 200.2902
+        """
+        Sépare une ligne de donnée reçue dans un tableau séparér en case pour le cas général (x, y, angle, autre)
+        Le cas spécifique est soit (x, y, angle, xMur, yMur, xMur, yMur,...) ou (x, y, angle, distance mur droite, distance mur gauche)
+        Retourne le tableau avec les données
+        """
+        tableauTemporaire = self.dataRecieved[0].split(";")
+        self.dataRecieved.remove(0)
+        return self.dataRecieved[0]
+    
     def getNumData(data : str):
+        """
+        prend ce que reçois l'ordi (x:200.2902) pour le séparer pour juste avoir 200.2902
+        """
         return float(data.split(":")[1])
